0de8892ed63bac60d69a3e67218b206bbe81f15e
Valentyn Bokhonov <v.bokhonov@serverpipe.com>
2021-04-15 08:31:50 +0300

    assignment3-task1

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+//Задача 1. Создать строку с своим именем, вывести количество символов содержащихся в ней.
+
+let myName = "Valentyn"
+print("Name \(myName) has \(myName.count) characters.")
