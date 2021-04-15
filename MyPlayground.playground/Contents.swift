a44d8db0eccf95f8c34958b8029e3d122151a30c
Valentyn Bokhonov <v.bokhonov@serverpipe.com>
2021-04-15 08:45:06 +0300

    assignment3-task2

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -4,3 +4,18 @@ import UIKit
 
 let myName = "Valentyn"
 print("Name \(myName) has \(myName.count) characters.")
+
+//Задача 2. Создать строку с своим отчеством проверить его на окончание “ич/на”
+
+func checkSecondName (middleName: String) {
+    switch middleName.suffix(2) {
+    case "ич":
+        print("Middle name \(middleName) has suffix \"ич\".")
+    case "на":
+        print("Middle name \(middleName) has suffix \"на\".")
+    default:
+        print("Middle name \(middleName) has some other suffix.")
+    }
+}
+
+checkSecondName(middleName: "Валентинович")
